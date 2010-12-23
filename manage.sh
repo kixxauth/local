3956548dcd2ea7454cb65ce03028b50cbe3d499b
Kristoffer Walker <kixxauth@gmail.com>
2010-12-23 14:10:11 -0500

    Management script is done through etc backup
     On branch master
    
    	modified:   manage.sh

--- manage.sh
+++ manage.sh
@@ -38,6 +38,9 @@ $PROJECTS_DIR \
 $GITHUB_DIR\
 "
 
+# Handy to have around
+DATE_TODAY="$(date +%Y-%m-%d)"
+
 for DIR in $DIRECTORIES
 do
     if ! [ -d $DIR ]; then
@@ -48,14 +51,56 @@ done
 
 get_local_git_repo ()
 {
-
-    if ! [ -d $GITHUB_DIR/local ]; then
+    if [ -d $GITHUB_DIR/local ]; then
+        echo 'deleting previous '$GITHUB_DIR/local
         chmod -R 777 $GITHUB_DIR/local
         rm -r $GITHUB_DIR/local
     fi
+    echo 'cloning git@github.com:kixxauth/local.git'
     cd $GITHUB_DIR
     git clone git@github.com:kixxauth/local.git
 }
 
-exit 0
+backup_etc ()
+{
+    if [ "$(id -u)" != "0" ]; then
+        echo 'this function must be run as root'
+        exit 1
+    fi
+    namex="bak_"
+    if [ -n $1 ]; then
+        namex="$namex$1_"
+    fi
+
+    full_name=$BAK_DIR/etc-$namex$DATE_TODAY.tar.bz2
+    echo 'backing /etc up to '$full_name
+    tar -cjf $full_name /etc
+}
+
+exit_msg ()
+{
+    echo "try:"
+    echo "bootstrap - download the local git repo"
+    echo "etcbak - backup the /etc dir (pass file name)"
+}
+
+if [ -z $1 ]; then
+    echo "exiting without running anything "
+    exit_msg
+    exit 0
+fi
+
+if [ $1 = 'bootstrap' ]; then
+    get_local_git_repo
+    exit 0
+fi
+
+if [ $1 = 'etcbak' ]; then
+    backup_etc $2
+    exit 0
+fi
+
+echo "invalid function call: "$1
+exit_msg
+exit 1
 
