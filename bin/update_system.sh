c6bc673e65b0bd7dffd4abfb3c53238fd46628f9
Kristoffer Walker <kris@kixx.name>
2012-01-06 18:22:05 -0500

    v2: updating ubuntu
     On branch v2
    
    	new file:   bin/manage
    	new file:   bin/update_system.sh
    	new file:   conf/conf.sh

---
+++ bin/update_system.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+super_or_fail 'to update system'
+
+apt-get update \
+    || fail "Unable to update system"
+apt-get dist-upgrade --fix-missing --assume-yes \
+    || fail "Unable to dist-upgrade system"
---
+++ conf/conf.sh
@@ -0,0 +1,12 @@
+UBUNTU_PACKAGES="\
+build-essential \
+vim-gnome \
+git-core \
+subversion \
+mercurial \
+ncftp \
+tree \
+keepassx \
+irssi \
+gimp \
+"
