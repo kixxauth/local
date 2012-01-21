ddbb1087a36a005af34f7b66e8c67f66f9e8b4f4
Kristoffer Walker <kris@kixx.name>
2012-01-20 19:31:25 -0500

    fix for remote reference repositories
     On branch master
    
    	new file:   conf/git_repository.list
    	modified:   lib/update_reference_projects.rb

--- lib/update_reference_projects.rb
+++ lib/update_reference_projects.rb
@@ -1,13 +1,22 @@
 HOME = File.expand_path('~')
 REFDIR = "#{HOME}/Rprojects/Reference"
-REPO_LIST = "#{REFDIR}/git_repository.list"
 RX_REPO = /(?<dir>[^\/]+)\.git$/
+LIBDIR = File.dirname(__FILE__)
+ROOTDIR = File.dirname(LIBDIR)
+CONFDIR = File.join(ROOTDIR, 'conf')
+REPO_LIST = File.join(CONFDIR, 'git_repository.list')
+
+puts LIBDIR
 
 begin
     fd = File.open(REPO_LIST, 'r')
 rescue
     puts "It looks like #{REPO_LIST} does not exist yet."
     Process.exit 1
+end
+
+if not File.directory? REFDIR
+    Dir.mkdir REFDIR
 end
 
 fd.each do |line|
