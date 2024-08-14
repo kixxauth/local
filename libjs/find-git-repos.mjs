b6fa2d3d462029bc3531e479b36d74282531725d
Kris Walker <kris@kixx.name>
2024-08-14 19:07:57 -0400

    Hook the git-repos script into ksys sub
     Changes to be committed:
    	modified:   libexec/ksys-dotfiles
    	new file:   libexec/ksys-git-repos
    	renamed:    scripts/encrypt-files/README.md -> libjs/encrypt-files/README.md
    	renamed:    scripts/encrypt-files/cli.mjs -> libjs/encrypt-files/cli.mjs
    	renamed:    scripts/encrypt-files/commands/decrypt-files.mjs -> libjs/encrypt-files/commands/decrypt-files.mjs
    	renamed:    scripts/encrypt-files/commands/encrypt-files.mjs -> libjs/encrypt-files/commands/encrypt-files.mjs
    	renamed:    scripts/encrypt-files/commands/generate-encryption-key.mjs -> libjs/encrypt-files/commands/generate-encryption-key.mjs
    	renamed:    scripts/find-git-repos.mjs -> libjs/find-git-repos.mjs

--- libjs/find-git-repos.mjs
+++ libjs/find-git-repos.mjs
@@ -5,6 +5,13 @@ import path from 'node:path';
 import fsp from 'node:fs/promises';
 
 
+// Pass in a path to the root of the directory tree as the
+// single positional commandline argument.
+if (!process.argv[2]) {
+    console.error('A root directory path is required as the only argument.');
+    process.exit(1);
+}
+
 const ROOT_DIR = path.resolve(process.argv[2]);
 
 const URLS = new Set();
@@ -14,6 +21,8 @@ function main() {
     return processNode(ROOT_DIR);
 }
 
+// Walk the directory tree and process each node. If we find a .git/ directory
+// then we extract the git URL and print it out to stdout.
 async function processNode(filepath) {
     const stats = await getStats(filepath);
 
@@ -61,7 +70,7 @@ async function processGitDirectory(filepath) {
         }
     }
 
-    console.error('!!NO URL in', filepath);
+    console.error('!!Missing expected Git URL in', filepath);
 }
 
 function getStats(filepath) {
